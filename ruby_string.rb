30dd97d0d445f5e20d317438b4d67cc2652a0885
ETSGitHub <etschulte@gmail.com>
2018-07-23 20:54:20 +0000

    automatically backed up by learn

---
+++ ruby_string.rb
@@ -0,0 +1,18 @@
+puts "Please type the guest name"
+guest_name = gets.chomp.capitalize
+puts "What is the name of your party?"
+party_name = gets.chomp
+puts "What is the date of your party?"
+date = gets.chomp
+puts "When is the party?"
+time = gets.chomp
+puts "What is your name?"
+host_name = gets.chomp.capitalize
+
+puts "Dear #{guest_name},
+ 
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
+ 
+Sincerely,
+ 
+Harry Potter"
